7560f78583c2b4b71515d7d4608f63385077e281
Daniel Garcia-Gil <danielgarciagil@gmail.com>
2025-10-30 08:14:52 -0400

    Actualizar la configuración de WorkerPool en Railtie y aumentar la versión a 0.1.1

--- lib/worker_pool/railtie.rb
+++ lib/worker_pool/railtie.rb
@@ -7,8 +7,8 @@ module WorkerPool
     initializer "worker_pool.configure" do |app|
       WorkerPool.configure do |c|
         c.workers  = app.config.worker_pool.fetch(:workers, c.workers)
-        c.in_buf   = app.config.worker_pool.fetch(:in_buf, c.in_buf)
-        c.work_buf = app.config.worker_pool.fetch(:work_buf, c.work_buf)
+        c.queue_size   = app.config.worker_pool.fetch(:queue_size, c.queue_size)
+        c.max_attempts = app.config.worker_pool.fetch(:max_attempts, c.max_attempts)
         c.logger   = app.config.worker_pool.fetch(:logger, c.logger)
       end
     end
